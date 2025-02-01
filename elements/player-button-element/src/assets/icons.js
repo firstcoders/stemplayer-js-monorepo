0cf5caccf2c33ac651b3684d7396e6addb72de69
Mark Cremer <mark@firstcoders.co.uk>
2025-02-01 12:28:04 +0000

    feat(player-button-element): added collapse

--- elements/player-button-element/src/assets/icons.js
+++ elements/player-button-element/src/assets/icons.js
@@ -120,7 +120,7 @@ export const loop = html`<svg
   />
 </svg>`;
 
-export const zoomin = html` <svg
+export const zoomin = html`<svg
   xmlns="http://www.w3.org/2000/svg"
   height="24px"
   viewBox="0 0 24 24"
@@ -134,7 +134,7 @@ export const zoomin = html` <svg
   <path d="M12 10h-2v2H9v-2H7V9h2V7h1v2h2v1z" />
 </svg>`;
 
-export const zoomout = html` <svg
+export const zoomout = html`<svg
   xmlns="http://www.w3.org/2000/svg"
   height="24px"
   viewBox="0 0 24 24"
@@ -144,5 +144,29 @@ export const zoomout = html` <svg
   <path d="M0 0h24v24H0V0z" fill="none" />
   <path
     d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7z"
+  />
+</svg>`;
+
+export const unfoldmore = html`<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="24px"
+  viewBox="0 -960 960 960"
+  width="24px"
+  fill="#5f6368"
+>
+  <path
+    d="M480-120 300-300l58-58 122 122 122-122 58 58-180 180ZM358-598l-58-58 180-180 180 180-58 58-122-122-122 122Z"
+  />
+</svg>`;
+
+export const unfoldless = html`<svg
+  xmlns="http://www.w3.org/2000/svg"
+  height="24px"
+  viewBox="0 -960 960 960"
+  width="24px"
+  fill="#5f6368"
+>
+  <path
+    d="m356-160-56-56 180-180 180 180-56 56-124-124-124 124Zm124-404L300-744l56-56 124 124 124-124 56 56-180 180Z"
   />
 </svg>`;
