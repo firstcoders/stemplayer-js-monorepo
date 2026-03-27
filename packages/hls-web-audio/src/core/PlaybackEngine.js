74923da6ba2933317ab33ae9a3f1482c31bbf344
Mark Cremer <mark@firstcoders.co.uk>
2026-03-27 19:05:37 +0000

    refactor(hls-web-audio): decouple AudioController god-object into domain facets

---
+++ packages/hls-web-audio/src/core/PlaybackEngine.js
@@ -0,0 +1,74 @@
+export default class PlaybackEngine {
+  constructor(controller) {
+    this.controller = controller;
+    this.isBuffering = false;
+    this.desiredState = 'suspended';
+    this.tTick = null;
+    this.refreshRate = controller.refreshRate || 250;
+  }
+
+  async play() {
+    this.desiredState = 'resumed';
+
+    if (typeof this.controller.duration !== 'number') throw new Error('Cannot play before loading content');
+    if (this.isBuffering) throw new Error('The player is buffering');
+
+    if (this.controller.ac.state === 'suspended') {
+      await this.controller.ac.resume();
+    }
+
+    if (typeof this.controller.timeline.adjustedStart !== 'number') {
+      this.controller.timeline.fixAdjustedStart(this.controller.offset);
+    }
+
+    this.controller.fireEvent('start');
+  }
+
+  async pause() {
+    this.desiredState = 'suspended';
+    if (this.controller.ac.state !== 'suspended') await this.controller.ac.suspend();
+    this.controller.fireEvent('pause');
+  }
+
+  tick() {
+    if (this.tTick) this.untick();
+
+    if (this.controller.currentTime > this.controller.offset + this.controller.playDuration) {
+      return this.controller.end();
+    }
+
+    this.controller.fireEvent('timeupdate', {
+      t: this.controller.currentTime,
+      pct: this.controller.pct,
+      remaining: this.controller.remaining,
+      act: this.controller.ac.currentTime,
+    });
+
+    if (this.controller.ac.state === 'running') {
+      this.tTick = setTimeout(() => this.tick(), this.refreshRate);
+    }
+  }
+
+  untick() {
+    if (this.tTick) clearTimeout(this.tTick);
+    this.tTick = null;
+  }
+
+  bufferingStart() {
+    this.controller.fireEvent('pause-start');
+    this.isBuffering = true;
+    if (this.controller.ac.state === 'running') this.controller.ac.suspend();
+  }
+
+  bufferingEnd() {
+    if (this.desiredState === 'resumed') this.controller.ac.resume();
+    this.isBuffering = false;
+    this.controller.fireEvent('pause-end');
+  }
+
+  async reset() {
+    await this.pause();
+    this.controller.timeline.adjustedStart = undefined;
+    this.desiredState = 'suspended';
+  }
+}
---
+++ packages/hls-web-audio/src/core/PlaybackTimeline.js
@@ -0,0 +1,108 @@
+import Timeframe from './Timeframe.js';
+
+export default class PlaybackTimeline {
+  #offset = 0;
+  #playDuration;
+
+  constructor(controller) {
+    this.controller = controller; // Reference back to dispatcher context
+    this.adjustedStart = undefined;
+    this.timeframe = new Timeframe();
+  }
+
+  get audioDuration() { return this.controller.trackGroup.audioDuration; }
+
+  set playDuration(duration) {
+    if (duration && typeof duration !== 'number')
+      throw new TypeError('The property "playDuration" must be of type number');
+    this.#playDuration = duration;
+    this.controller.notifyUpdated('playDuration', this.playDuration);
+  }
+
+  get playDuration() {
+    return this.#playDuration || this.audioDuration;
+  }
+
+  set offset(offset = 0) {
+    if (typeof offset !== 'number')
+      throw new TypeError('The property "offset" must be of type number');
+    this.#offset = offset;
+    this.controller.notifyUpdated('offset', this.offset);
+  }
+
+  get offset() {
+    return this.#offset;
+  }
+
+  get rawCurrentTime() {
+    return this.adjustedStart !== undefined ? this.controller.ac.currentTime - this.adjustedStart : undefined;
+  }
+
+  get currentTime() {
+    if (this.rawCurrentTime < this.offset) {
+      this.fixAdjustedStart(this.offset);
+    }
+    if (this.controller.loop && this.rawCurrentTime >= this.offset + this.playDuration) {
+      this.fixAdjustedStart(this.offset);
+    }
+    return this.rawCurrentTime;
+  }
+
+  setCurrentTime(t) {
+    if (typeof this.audioDuration !== 'number' || t < 0 || t > this.audioDuration)
+      throw new Error(`CurrentTime ${t} should be between 0 and duration ${this.audioDuration}`);
+
+    let seekTo = t;
+    if (seekTo < this.offset || seekTo > this.offset + this.playDuration) {
+      seekTo = this.offset;
+    }
+
+    this.fixAdjustedStart(seekTo);
+
+    this.controller.ac.suspend().then(() => {
+      if (this.controller.desiredState === 'resumed' && !this.controller.engine.isBuffering) {
+        this.controller.ac.resume();
+      }
+    });
+  }
+
+  get currentTimeframe() {
+    return this.timeframe.update({
+      adjustedStart: this.adjustedStart,
+      adjustedEnd: this.adjustedEnd,
+      currentTime: this.currentTime,
+      playDuration: this.playDuration,
+      offset: this.offset,
+    });
+  }
+
+  fixAdjustedStart(t) {
+    this.adjustedStart = this.controller.ac.currentTime - t;
+    this.controller.fireEvent('seek', {
+      t: this.currentTime,
+      pct: this.pct,
+      remaining: this.remaining,
+    });
+  }
+
+  set pct(n) {
+    if (this.audioDuration) {
+      let factor = n;
+      if (factor < 0) factor = 0;
+      if (factor > 1) factor = 1;
+      this.setCurrentTime(factor * this.audioDuration);
+    }
+  }
+
+  get pct() {
+    return this.currentTime / this.audioDuration;
+  }
+
+  get remaining() {
+    return this.audioDuration - this.currentTime;
+  }
+
+  get adjustedEnd() {
+    return this.adjustedStart + this.offset + this.playDuration;
+  }
+}
