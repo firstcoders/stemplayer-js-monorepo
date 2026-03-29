773bfea0e26b06b3029dfd2bd3f5f7682a1281e4
Mark Cremer <mark@firstcoders.co.uk>
2026-03-29 07:23:17 +0100

    chore: upped eslint

---
+++ elements/slider-element/eslint.config.js
@@ -0,0 +1,23 @@
+import openWcConfig from '@open-wc/eslint-config';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'coverage/**',
+      'dist/**',
+      'out-tsc/**',
+      '_site/**',
+      '.tmp/**',
+    ],
+  },
+
+  ...openWcConfig,
+
+  {
+    rules: {
+      ...prettierConfig.rules,
+    },
+  },
+];
